4958183ad0e623e906e699783ffcce8384b01872
Ahmad Hisham <ahmad.hisham@gmail.com>
2018-07-30 19:47:11 +0300

    Move root route to a route file & view

--- start.js
+++ start.js
@@ -1,8 +1,14 @@
 var express = require("express");
+var path = require("path");
+
 var app = express();
 
-app.get("/", function(req, res){
-  res.send("Hello world!");
-});
+// view engine setup
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+// Root route
+var indexRouter = require("./routes/index");
+app.use("/", indexRouter);
 
 app.listen(3000);
